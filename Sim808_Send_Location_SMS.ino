1cb9e2f15389140bb7185948b797d3df436292e2
Taiki Beses <83702482+ta2bss@users.noreply.github.com>
2023-12-11 21:52:26 +0300

    Update Sim808_Send_Location_SMS.ino

--- Sim808_Send_Location_SMS.ino
+++ Sim808_Send_Location_SMS.ino
@@ -1,6 +1,10 @@
-/* If you send an SMS by typing "Location" to the phone line connected to the device, the device will send you its location.
+/* 
+If you send an SMS by typing "Location" to the phone line connected to the device, the device will send you its location.
 Additionally, it will send you its location via SMS at the time intervals you enter below.
-Don't forget to put gsm number to send sms. */
+Don't forget to put gsm number to send sms. 
+
+https://github.com/ta2bss/sim808-Location-SMS
+*/
 
 
 #include <SoftwareSerial.h>
